841f3c3dcf3a3caa2760664b9a86d917779ea629
Pedro Rodríguez <pemarosan@gmail.com>
2026-06-28 20:48:24 +0200

    Update filtrado_variantes_enhancers_ATP2B4.sh

--- Pipelines/filtrado_variantes_enhancers_ATP2B4.sh
+++ Pipelines/filtrado_variantes_enhancers_ATP2B4.sh
@@ -9,7 +9,8 @@
 # Detener el script si ocurre algún error
 set -e
 
-# --- 1. Definición de Variables y URL ---
+# --- 1. DEFINICION DE VARIABLES Y URL ---
+# --------------------------------------------------------------
 URL_VCF="https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20201028_3202_raw_GT_with_annot/20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chr1.recalibrated_variants.vcf.gz"
 REGIONES="chr1:203674063-203674413,chr1:203682652-203683001,chr1:203650434-203650784,chr1:203682483-203682652,chr1:203628800-203629147,chr1:203629509-203629793,chr1:203626961-203627130,chr1:203673776-203673947,chr1:203650110-203650300,chr1:203655610-203675957,chr1:203653169-203653514,chr1:203629172-203629428"
 
@@ -55,3 +56,35 @@ bcftools view \
   -Ov \
   -o ATP2B4_enhancers_USA.vcf \
   ATP2B4_enhancers_filtrado.recode.vcf
+
+
+# 2. PROCESAMIENTO Y COMBINACION DE VCFS (MSL & USA) PARA UTILIZARLO EN PCADAPT
+# -------------------------------------------------------------------------------
+echo "Comprimiendo archivos VCF..."
+bgzip -c ATP2B4_enhancers_MSL.vcf > ATP2B4_enhancers_MSL.vcf.gz
+bgzip -c ATP2B4_enhancers_USA.vcf > ATP2B4_enhancers_USA.vcf.gz
+
+# 2. INDEXAR LOS ARCHIVOS COMPRIMIDOS
+# -------------------------------------------------------------------------------
+echo "Indexando archivos VCF comprimidos..."
+bcftools index ATP2B4_enhancers_MSL.vcf.gz
+bcftools index ATP2B4_enhancers_USA.vcf.gz
+
+# 3. FUSIONAR AMBOS ARCHIVOS EN UN SOLO VCF CONJUNTO
+# -------------------------------------------------------------------------------
+echo "Fusionando poblaciones (Sierra Leona + USA)..."
+bcftools merge \
+  -Ov \
+  -o ATP2B4_enhancers_USA_MSL.vcf \
+  ATP2B4_enhancers_USA.vcf.gz \
+  ATP2B4_enhancers_MSL.vcf.gz
+
+# 4. CONVERTIR EL ARCHIVO CONJUNTO A FORMATO PLINK (.bed/.bim/.fam)
+# -------------------------------------------------------------------------------
+echo "Convirtiendo VCF final a formato binario de PLINK..."
+plink --vcf ATP2B4_enhancers_USA_MSL.vcf \
+      --keep-allele-order \
+      --make-bed \
+      --out F:/UNIVERSIDAD/UNIR/TFM/ARCHIVOS/ATP2B4_USA_MSL
+
+echo "¡Proceso completado con éxito!"
